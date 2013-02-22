4d84711a59f5f45b2aa5de39bd5bc8849601d3cb
luckxudong <LuckXudong@gmail.com>
2013-02-22 08:35:58 +0000

    refactor AbstractRenderer

---
+++ src/com/ice/common/SceneRenderer.java
@@ -0,0 +1,23 @@
+package com.ice.common;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+/**
+ * User: jason
+ * Date: 13-2-22
+ */
+public class SceneRenderer extends AbstractRenderer {
+
+    @Override
+    protected void onCreated(EGLConfig config) {
+    }
+
+    @Override
+    protected void onChanged(int width, int height) {
+    }
+
+    @Override
+    protected void onFrame() {
+    }
+
+}
